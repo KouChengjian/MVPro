9cd88e242cfc69259d41760df9b455b7f5987ec0
KouChengjian <hui793114465@163.com>
2016-09-04 20:17:05 +0800

    init

---
+++ mvpro/src/org/loader/presenter/ActivityPresenterImpl.java
@@ -0,0 +1,63 @@
+package org.loader.presenter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.loader.helper.GenericHelper;
+import org.loader.view.IView;
+
+/**
+ * 将Activity作为Presenter的基类 <br />
+ * Created by qibin on 2015/11/15.
+ */
+public class ActivityPresenterImpl<T extends IView> extends Activity implements IPresenter<T> {
+
+    protected T mView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        create(savedInstanceState);
+
+        try {
+            mView = getViewClass().newInstance();
+            mView.bindPresenter(this);
+            setContentView(mView.create(getLayoutInflater(), null));
+            mView.bindEvent();
+            created(savedInstanceState);
+        } catch(Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+    
+/**    
+     @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (mView == null) {
+            try {
+                mView = getViewClass().newInstance();
+            } catch (InstantiationException e) {
+                throw new RuntimeException("create IDelegate error");
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException("create IDelegate error");
+            }
+        }
+    }
+*/
+
+    @Override
+    public Class<T> getViewClass() {
+        return GenericHelper.getViewClass(getClass());
+    }
+
+    @Override
+    public void create(Bundle savedInstance) {
+
+    }
+
+    @Override
+    public void created(Bundle savedInstance) {
+
+    }
+}
